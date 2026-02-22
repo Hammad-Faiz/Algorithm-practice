74506a44dee652da22a41152e8f6d19f7ce2bd1f
Hammad Faiz <hammadfaiz172@gmail.com>
2026-02-22 11:25:15 -0500

    completed exercise 3

---
+++ java-core/Exercise04_Recursion.java
@@ -0,0 +1,37 @@
+// Exercise 04 - Recursion
+// ----------------------------------------
+// TODO 1: Write a recursive method called factorial(int n)
+//         that returns n! (n factorial)
+//         factorial(5) should return 120
+//         Remember: base case is n == 1, return 1
+
+// TODO 2: Write a recursive method called sumDownTo(int n)
+//         that adds all numbers from n down to 1
+//         sumDownTo(5) should return 15 (5+4+3+2+1)
+
+// TODO 3: Write a recursive method called countDown(int n)
+//         that prints each number from n down to 1, then prints "Go!"
+
+public class Exercise04_Recursion {
+
+    // --- TODO 1 ---
+    public static int factorial(int n) {
+
+    }
+
+    // --- TODO 2 ---
+    public static int sumDownTo(int n) {
+
+    }
+
+    // --- TODO 3 ---
+    public static void countDown(int n) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));   // should print 120
+        System.out.println(sumDownTo(5));   // should print 15
+        countDown(5);                       // should print 5 4 3 2 1 Go!
+    }
+}
